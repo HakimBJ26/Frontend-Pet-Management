c81f65286e5a6d335c492720d1700123d8318a96
aymenAbessi <aymenabassi78@gmail.com>
2024-07-15 15:15:58 +0200

    thread resolved

--- src/common/configuration/constants/Paths.jsx
+++ src/common/configuration/constants/Paths.jsx
@@ -1,6 +1,6 @@
-export const SIGN_UP_PATH = '/signup';
-export const SIGN_IN_PATH = '/signin';
-export const ADMIN_DASH_PATH = '/dashboard-admin';
-export const USER_MANAGEMENT_PATH = '/users-management';
-export const CLIENT_DASH_PATH = '/dashboard-client';
-export const VETO_DASH_PATH = '/dashboard-veterinarian';
+export const SIGN_UP_PATH = '/sign_up';
+export const SIGN_IN_PATH = '/sign_in';
+export const ADMIN_DASH_PATH = '/dashboard_admin';
+export const USER_MANAGEMENT_PATH = '/users_management';
+export const CLIENT_DASH_PATH = '/dashboard_client';
+export const VETO_DASH_PATH = '/dashboard_veterinarian';
